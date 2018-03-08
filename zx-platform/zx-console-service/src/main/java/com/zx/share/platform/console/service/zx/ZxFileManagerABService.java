0dcdcd79fb4a2e4901449c14a212ddb0204dec5c
wangyaomin1 <413667794@qq.com>
2018-03-08 12:40:21 +0800

    1111111

--- zx-platform/zx-console-service/src/main/java/com/zx/share/platform/console/service/zx/ZxFileManagerABService.java
+++ zx-platform/zx-console-service/src/main/java/com/zx/share/platform/console/service/zx/ZxFileManagerABService.java
@@ -1,5 +1,17 @@
 package com.zx.share.platform.console.service.zx;
 
+import com.zx.share.platform.bean.zx.ZxFileManagerAB;
+import com.zx.share.platform.bean.zx.ZxPrinterManager;
+import com.zx.share.platform.util.response.DefaultResopnseBean;
+
 public interface ZxFileManagerABService {
 
+	/**
+	 * 
+	 * @Title: add
+	 * @Description: 添加文件分类
+	 * @param zxAB
+	 *            添加文件分类
+	 */
+	public DefaultResopnseBean<Object> add(ZxFileManagerAB zxAB);
 }
