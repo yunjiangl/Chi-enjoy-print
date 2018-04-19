d4f7a9e415197c81176accf1096680f3bc200f0c
wangyaomin1 <413667794@qq.com>
2018-04-19 10:34:07 +0800

    b文件的遍历和删除

--- zx-platform/platform/src/main/resources/static/js/modules/file/B.js
+++ zx-platform/platform/src/main/resources/static/js/modules/file/B.js
@@ -9,6 +9,11 @@ $(function () {
 			{ label: '文件分类', name: 'sysDictionary.name',  width: 80 },
 			{ label: '打印次数', name: 'fileNum', width: 80 },
             { label: '页数', name: 'fileNum', width: 80 },
+            { label: '操作', name: 'id',width: 80, formatter: function (value, options, row) { 
+                return '<a class="btn btn-primary btn-sm" href="javascript:;" onclick="deleteFileB(\''+value + '\');">删除</a>&nbsp;&nbsp;'               
+                +'<a class="btn btn-primary btn-sm" href="javascript:;" onclick="updateFileB(\''+value + '\')">修改</a>';
+            } },
+
         ],
 		viewrecords: true,
         height: 385,
@@ -44,124 +49,12 @@ var vm = new Vue({
         	fileName:null
         },
 		showList: true,
-		title: null,
+		title: null
 	},
+	
 	methods: {
 		query: function () {
 			vm.reload();
-		},
-		add: function(){
-			vm.showList = false;
-			vm.title = "新增";
-			vm.user = {};
-		},
-		
-		deleteFileB:function(){
-			var userIds = getSelectedRows();
-			console.log(userIds);
-			if(userIds == null){
-				return ;
-			}
-			confirm('确定要删除该文件吗？', function(){
-				$.ajax({
-					type: "POST",
-				    url: baseURL + "zx/ab/sys/deleteUserById?Id='"+userIds+"'",
-                    contentType: "application/json",
-				    success: function(r){
-				    	console.log(r);
-						if(r.code == 0){
-							alert('删除成功', function(index){
-								$("#jqGrid")。
-							});
-						}else{
-							alert('删除失败');
-						}
-					}
-				});
-			});
-		},
-		update: function (event) {
-			var userId = getSelectedRow();
-			if(userId == null){
-				return ;
-			}
-			vm.showList = false;
-            vm.title = "修改";
-            
-            vm.getInfo(userId)
-		},
-		saveOrUpdate: function (event) {
-			var url = vm.user.userId == null ? "user/save" : "user/update";
-			$.ajax({
-				type: "POST",
-			    url: baseURL + url,
-                contentType: "application/json",
-			    data: JSON.stringify(vm.user),
-			    success: function(r){
-			    	if(r.code === 0){
-						alert('操作成功', function(index){
-							vm.reload();
-						});
-					}else{
-						alert(r.msg);
-					}
-				}
-			});
-		},
-		deleteAttorney: function (event) {
-			var userIds = getSelectedRows();
-			if(userIds == null){
-				return ;
-			}
-			
-			confirm('确定要禁用选中的账号？', function(){
-				$.ajax({
-					type: "POST",
-				    url: baseURL + "user/delete",
-                    contentType: "application/json",
-				    data: JSON.stringify(userIds),
-				    success: function(r){
-						if(r.code == 0){
-							alert('操作成功', function(index){
-								$("#jqGrid").trigger("reloadGrid");
-							});
-						}else{
-							alert(r.msg);
-						}
-					}
-				});
-			});
-		},
-        checkAttorney: function (event) {
-            var userIds = getSelectedRows();
-            if(userIds == null){
-                return ;
-            }
-            layer.open({
-                type: 1,
-                offset: '50px',
-                skin: 'layui-layer-molv',
-                title: "审核",
-                area: ['300px', '450px'],
-                shade: 0,
-                shadeClose: false,
-                content: jQuery("#checkLayer"),
-                btn: ['确定', '取消'],
-                btn1: function (index) {
-                    // var node = ztree.getSelectedNodes();
-                    //选择上级菜单
-                    // vm.menu.parentId = node[0].menuId;
-                    // vm.menu.parentName = node[0].name;
-
-                    layer.close(index);
-                }
-            });
-            $("#jqGrid").trigger("reloadGrid");
-        },
-		getInfo: function(userId){
-			$.get(baseURL + "user/info/"+userId, function(r){
-                vm.user = r.user;
-            });
 		},
 		reload: function (event) {
 			vm.showList = true;
@@ -173,3 +66,32 @@ var vm = new Vue({
 		}
 	}
 });
+
+/**
+ * 删除文件
+ * @param id
+ * @returns
+ */
+function deleteFileB(id){
+	console.log(id);
+	window.confirm('是否确定删除！！！',function(){
+		console.log('asd');
+		$.ajax({
+		    url:baseURL + 'zx/ab/sys/deleteUserById?Id='+id,
+		    type:"GET",
+		    data:{
+		    	Id:id
+		    },
+		    async:false,
+		    success:function (res) {
+		      location.reload();
+		      parent.layer.alert("删除成功", function(index){
+		    	  window.parent.location.reload();
+		  	});
+		      
+		  		}
+		  	});
+		    
+		  
+	})
+}
