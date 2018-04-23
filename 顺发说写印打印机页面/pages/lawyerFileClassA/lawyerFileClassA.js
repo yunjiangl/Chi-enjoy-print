b7004a29518b12efe0df3f64f561b97ac917775a
yunjiangl <1205445409@qq.com>
2018-04-23 18:01:07 +0800

    律师查找文件标题的联动

--- 顺发说写印打印机页面/pages/lawyerFileClassA/lawyerFileClassA.js
+++ 顺发说写印打印机页面/pages/lawyerFileClassA/lawyerFileClassA.js
@@ -5,28 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    navbar:['民事案由','刑事罪名','行政案由'],
-    currentTab:0,
-    listNavBar:['民事','商事','知识产权','企业破产清算','破坏社会主义市场经济罪','侵犯公民人身权利、民主权利罪'],
-    listTitleTab:0,
-  
+    navbar: null,
+    currentTab: 0,
+    listNavBar: null,
+    listTitleTab: 0,
+    listUntilData: null
   },
-  navbarTap:function(e){
+  navbarTap: function (e) {
+    var that = this;
     this.setData({
-      currentTab:e.currentTarget.dataset.idx
+      currentTab: e.currentTarget.dataset.idx,
+      listNavBar: that.data.navbar[e.currentTarget.dataset.idx].list,
+      listUntilData: that.data.navbar[e.currentTarget.dataset.idx].list[0].list
     });
   },
-  listNavBarTab:function(e){
+  listNavBarTab: function (e) {
+    var that = this;
+   
     this.setData({
-      listTitleTab: e.currentTarget.dataset.idx
+      listTitleTab: e.currentTarget.dataset.idx,
+      listUntilData: that.data.listNavBar[e.currentTarget.dataset.idx].list
     })
   },
-  trun:function(){
-    wx:wx.navigateTo({
-      url: '../lawyerFourFileList/lawyerFourFileList',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
+  trun: function (e) {
+    var that = this;
+    console.log(that.data.listUntilData[e.currentTarget.dataset.idx])
+    wx: wx.navigateTo({
+      url: '../lawyerFourFileList/lawyerFourFileList?id=' + that.data.listUntilData[e.currentTarget.dataset.idx].id,
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
     })
   },
 
@@ -34,55 +42,69 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+
+    wx.request({
+      url: app.data.api + 'dictionary/a/list',
+      method: 'GET',
+      success: function (data) {
+
+        that.setData({
+          navbar: data.data.data[0].list,
+          listNavBar: data.data.data[0].list[0].list,
+          listUntilData: data.data.data[0].list[0].list[0].list
+        })
+        
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
