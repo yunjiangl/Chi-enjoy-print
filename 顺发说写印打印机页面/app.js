e2006f9e4eba69322b2c67c951e671cac58a0bbe
feng gang <fglovezzr@163.com>
2018-04-24 16:43:13 +0800

    修改app.js

--- 顺发说写印打印机页面/app.js
+++ 顺发说写印打印机页面/app.js
@@ -29,55 +29,64 @@ App({
     wx.getUserInfo({
       success: function (res) {
         // console.log(res);
-        wx.login({
-          success: function (loginres) {
-            wx.request({
-              url: that.data.api + 'wechat/login',
-              data: {
-                appId: '',
-                code: loginres.code,
-                iv: res.iv,
-                encryptedData: res.encryptedData
-              },
-              method: 'GET',
-              header: {
-                //'content-type': 'X-ACCESS-TOKEN'
-              },
-              success: function (data) {
-                console.log(data.data);
-                // 如果用户刚刚使用微信登录
-                if (data.data.data.userStatus == 1) {
-                  wx.redirectTo({
-                    url: './register'
-                  })
-                 
-                } else {
-                  // 判断用户类型
+        wx.getUserInfo({
+          success: function (res) {
+            wx.login({
+              success: function (loginres) {
+                wx.request({
+                  url: that.data.api + that.data.urlWechatLogin,
+                  data: {
+                    appId: '',
+                    code: loginres.code,
+                    iv: res.iv,
+                    encryptedData: res.encryptedData
+                  },
+                  method: 'GET',
+                  header: {
+                    //'content-type': 'X-ACCESS-TOKEN'
+                  },
+                  success: function (response) {
 
-                  if (data.data.data.userType == 2) {
-                    //跳转到律师首页
-                    wx.redirectTo({
-                      url: './lawyerIndex'
-                    })
-                   
-                  } else if (data.data.data.userType == 1) {
-                    //跳转到普通用户首页
-                    wx.redirectTo({
-                      url: './index'
-                    })
-                    
-                  }
-                }
+                    console.log("1");
+                    console.log(response);
+                    console.log("2");
+                    console.log(response.data.code);
+                    console.log("3");
+                    console.log(response.data.data);
+                    console.log("4");
+                    console.log(response.data.data.userType);
+                    // 如果用户刚刚使用微信登录
 
-                // 为全局变量赋值
-                that.data.userInfo.userCode = data.data.data.userCode;
-                that.data.userInfo.userType = data.data.data.userType;
-                that.data.userInfo.accessToken = data.data.data.accessToken;
+                    if (response.data.code == 200) {
+                        // 判断用户类型
+
+                      if (response.data.data.userType == 2) {
+                          //跳转到律师首页
+                          wx.redirectTo({
+                            url: '../lawyerIndex/lawyerIndex'
+                          })
+
+                      } else if (response.data.data.userType == 1) {
+
+                        console.log("5");
+                          //跳转到普通用户首页
+                          wx.redirectTo({
+                            url: '../lawyerIndex/lawyerIndex'
+                          })
+
+                        }
+                      }
+
+                      // 为全局变量赋值
+                      that.data.userInfo = response.data.data;
+                      console.log(that.data.userInfo);
+                    }
+                })
+
               }
             })
-
           }
-        })
+        })  
       }
     })
   }
