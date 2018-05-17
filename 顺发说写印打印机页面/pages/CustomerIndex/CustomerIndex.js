69344b35718bc4d305b53a6bff87746b93db1965
yunjiangl <1205445409@qq.com>
2018-05-17 14:05:57 +0800

    订单数据优化，客户上床手机图片

--- 顺发说写印打印机页面/pages/CustomerIndex/CustomerIndex.js
+++ 顺发说写印打印机页面/pages/CustomerIndex/CustomerIndex.js
@@ -51,15 +51,33 @@ Page({
       url: '../lawyerFileClassC/lawyerFileClassC',
     })
   },
-  uploadImg:function(){
+  uploadImg: function () {
     wx.chooseImage({
-      count: 9, // 默认9
+      count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var tempFilePaths = res.tempFilePaths
-        console.log(tempFilePaths)
+        wx.uploadFile({
+          url: app.data.api + app.data.urlUploadFileE,
+          filePath: tempFilePaths[0],
+          name: 'multipartFile',
+          header: {
+            'X-ACCESS-TOKEN': app.data.userInfo.accessToken
+          },
+          formData: {
+            'abstracts': '用户上传手机图片', // 摘要
+            'userCode': app.data.userInfo.userCode,//用户code
+          },
+          success: function (res) {
+            var data = JSON.parse(res.data);
+            console.log(data)
+            wx.navigateTo({
+              url: '../setPrint/setPrint?fileCode=' + data.data
+            })
+          }
+        })
       }
     })
   },
